4d2a9b0ac21837ed1dd198d83278a52f07e8e465
Andrey Mereskin <mereskin@gmail.com>
2014-11-22 22:20:44 -0800

    More efficient computation of numbers

--- react-custom-element/src/fibonacci-numbers.js
+++ react-custom-element/src/fibonacci-numbers.js
@@ -1,21 +1,20 @@
 /** @jsx React.DOM */
 var FibonacciNumbers = React.createClass({
   render: function(){
-    return <p>{ this.sequence(parseInt(this.props.upTo, 10)).join(', ') }</p>;
+    return <p>{ this.sequence(parseInt(this.props.limit, 10)).join(', ') }</p>;
   },
 
-  sequence: function(max) {
-    var results = [];
-    for(var i=0;i<max;i++) {
-      results.push(fibonacci(i));
+  sequence: function(limit) {
+    if(limit <= 0) return [];
+    if(limit == 1) return [1];
+
+    var results = [1, 1];
+    var max = Math.min(limit, 1001) - 1;
+    for(var i=2;i<max;i++) {
+      results[i] = results[i-2] + results[i-1];
     }
     return results;
   }
 });
-
-function fibonacci(n) {
-  if(n <= 1) return 1;
-  return fibonacci(n-1) + fibonacci(n-2);
-}
 
 versal.registerReactComponent('fibonacci-numbers', FibonacciNumbers);
